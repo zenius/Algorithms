e24b316e892394e91f3b2452f45a18ac3018eb14
zenius <zenius2050@gmail.com>
2019-09-04 22:15:04 +0530

    another method added for binary-search

--- binary-search.js
+++ binary-search.js
@@ -1,4 +1,4 @@
-// algorithms.js
+// Method I 
 
 function binarySearch(array, targetValue) {
 	let min = 0;
@@ -8,14 +8,39 @@ function binarySearch(array, targetValue) {
 	while(1) {
 		if(max < min) { return -1; } // target not found  
 		
-		if(array[guess] === targetValue) { return guess; } // target found 
-		else if(array[guess] < targetValue) { min =  guess + 1;}
-		else { max = guess - 1; }
-
+		if(array[guess] === targetValue) { 
+      // target found 
+      return guess; 
+    } 
+		else if(array[guess] < targetValue) { 
+      min =  guess + 1;
+    }
+		else { 
+      max = guess - 1; 
+    }
 		guess = Math.floor((max+min)/2);
 	}
 };
 
+// Method 2
+function binarySearch(array, targetValue) {
+	let min = 0;
+	let max = array.length - 1;
+	let guess = Math.floor((min+max)/2);
 
-
-
+	while(min<=max) {
+		if(array[guess] === targetValue) { 
+      // target found 
+      return guess; 
+    }
+		else if(array[guess] < targetValue) { 
+      min =  guess + 1;
+    }
+		else { 
+      max = guess - 1; 
+    }
+		guess = Math.floor((max+min)/2);
+	}
+  
+  return -1; 
+};
