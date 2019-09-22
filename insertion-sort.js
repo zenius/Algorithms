1bb8597b45e5059b7eb9d87c4112d4ff4ac5bba6
zenius <zenius2050@gmail.com>
2019-09-22 21:00:12 +0530

    insertion-sort code modified

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,23 +1,22 @@
 /*Challenge: Insert a Value Before an Index in Sorted Order*/
 
-var insert = function(array, rightIndex, value) {
-  var length = array.length; 
-  var positionToInsert = rightIndex + 1; 
-  for(var index = rightIndex ; index >= 0 && array[index] > value; index--) { 
-     // array[index] <= value is equivaluent to : array[index] > value 
-     if(array[index] <= value) { break;} 
-      positionToInsert = index; 
-      array[index+1] = array[index];
-  }
-  array[positionToInsert] = value; 
-};
+const insert = function(array, index) {
+  let value = array[index];
+  let j = index - 1; 
 
-
-var insertionSort = function(array) {
-  var length = array.length; 
-  for(var index = 1 ; index < length; index++) { 
-  	// remember to adjust the edge case 
-  	// hence, index starts with 1
-        insert(array, index-1, array[index]) 
+  while(j >= 0 && value < array[j]) {
+    // right shift operation
+    array[j + 1] = array[j]; 
+    j--; 
   }
-};
+  array[j + 1] = value; 
+}
+
+const insertionSort = function(array) {
+  const length = array.length; 
+
+  for (let rightIndex = 1; rightIndex < length; rightIndex++) { 
+    insert(array, rightIndex); 
+  }
+  return array; 
+}
