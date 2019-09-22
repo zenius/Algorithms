32af3f8aa59955f510e99d69d70474619c48a853
zenius <zenius2050@gmail.com>
2019-09-22 21:04:23 +0530

    code format updated

--- binary-search.js
+++ binary-search.js
@@ -1,47 +1,44 @@
 // Method I 
-
 function binarySearch(array, targetValue) {
-	let min = 0;
-	let max = array.length - 1;
-	let guess = Math.floor((min+max)/2);
+  let min = 0;
+  let max = array.length - 1;
+  let guess = Math.floor((min + max) / 2);
 
-	while(1) {
-		if(max < min) { return -1; } // target not found  
-		
-		if(array[guess] === targetValue) { 
+  while (1) {
+    if (max < min) {
+      return -1;
+    } // target not found  
+
+    if (array[guess] === targetValue) {
       // target found 
-      return guess; 
-    } 
-		else if(array[guess] < targetValue) { 
-      min =  guess + 1;
+      return guess;
+    } else if (array[guess] < targetValue) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
     }
-		else { 
-      max = guess - 1; 
-    }
-		guess = Math.floor((max + min)/2);
-	}
+    guess = Math.floor((max + min) / 2);
+  }
 };
 
 // Method 2
 function binarySearch(array, targetValue) {
-	let min = 0;
-	let max = array.length - 1;
-	let guess = Math.floor((min+max)/2);
+  let min = 0;
+  let max = array.length - 1;
+  let guess = Math.floor((min + max) / 2);
 
-	while(min <= max) {
-		if(array[guess] === targetValue) { 
+  while (min <= max) {
+    if (array[guess] === targetValue) {
       // target found 
-      return guess; 
+      return guess;
+    } else if (array[guess] < targetValue) {
+      min = guess + 1;
+    } else {
+      max = guess - 1;
     }
-		else if(array[guess] < targetValue) { 
-      min =  guess + 1;
-    }
-		else { 
-      max = guess - 1; 
-    }
-		guess = Math.floor((max + min)/2);
-	}
- 
-	// target not found
-  return -1; 
+    guess = Math.floor((max + min) / 2);
+  }
+
+  // target not found
+  return -1;
 };
