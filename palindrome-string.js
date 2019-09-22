e44761e0a7a68a765cf8fcfdfcf9066426227092
zenius <zenius2050@gmail.com>
2019-09-22 21:10:53 +0530

    code formatting updated

--- palindrome-string.js
+++ palindrome-string.js
@@ -1,22 +1,21 @@
-/*Is string a palindrome?*/
+const isPalindrome = function(string) {
+  const length = string.length;
 
-const isPalindrome = function (string) {
-	const length = string.length; 
+  if (length < 2) {
+    return true;
+  }
 
-	if(length < 2) {
-		return true; 
-	} 
+  if (string.charAt(0) !== string.charAt(length - 1)) {
+    return false;
+  }
 
-	if(string.charAt(0) !== string.charAt(length - 1)) {
-		return false;  
-	}
+  const newString = string.substring(1, length - 1);
 
-	const newString = string.substring(1, length-1); 
-
-	return isPalindrome(newString); 
+  return isPalindrome(newString);
 }
 
 console.log(isPalindrome("racecar")); // true 
 console.log(isPalindrome("motor")); // false 
 console.log(isPalindrome("rotor")); // true 
-console.log(isPalindrome("foobar")); // false 
+console.log(isPalindrome("foobar")); // false
+console.log(isPalindrome("")); // true 
